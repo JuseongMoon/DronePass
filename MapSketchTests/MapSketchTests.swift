4fb12b7a77134647810cb31464a4b17f39e9dfb2
JuseongMoon <37950716+JuseongMoon@users.noreply.github.com>
2025-05-13 12:39:01 +0900

    Initial Commit

---
+++ MapSketchTests/MapSketchTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapSketchTests.swift
+//  MapSketchTests
+//
+//  Created by 문주성 on 5/13/25.
+//
+
+import Testing
+@testable import MapSketch
+
+struct MapSketchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
